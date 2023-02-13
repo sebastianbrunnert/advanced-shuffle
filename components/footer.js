82e2d1b0588f77a27d127438fb604a2d342e3b8a
Sebastian Brunnert <se.brunnert@gmail.com>
2023-02-13 23:31:18 +0100

    Update direct links to Footer

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer className="pt-16 pb-4 text-center">
             <p>Developed by <a target="_blank" rel="noreferrer" href="https://sebastianbrunnert.me" className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-red-400">Sebastian Brunnert</a></p>
             <p>
-                <a target="_blank" rel="noreferrer" href="https://sebastianbrunnert.me/#4" className="hover:underline">Imprint</a><span> - </span><a target="_blank" rel="noreferrer" href="https://sebastianbrunnert.me/#5" className="hover:underline">Privacy</a>
+                <a target="_blank" rel="noreferrer" href="https://sebastianbrunnert.me#imprint" className="hover:underline">Imprint</a><span> - </span><a target="_blank" rel="noreferrer" href="https://sebastianbrunnert.me#privacy" className="hover:underline">Privacy</a>
             </p>
         </footer>
     );
